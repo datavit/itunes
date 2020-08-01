f6a1d99199fe27fc9d8ff760008a85bca3597b30
vit <vit.mitelman@gmail.com>
2020-08-01 14:23:24 +0300

    added js files

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,22 @@ import {videoPlayerInit} from './videoPlayer.js';
 
 const playerBtn =document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
 
-playerBtn.forEach((btn) => {
+
+const deactivationPlayer = () => {
+    temp.getElementsByClassName.display = 'none';
+    playerBtn.forEach((item) => {item.classList.remove('active') });
+    playerBlock.forEach((item) => {item.classList.remove('active') });
+}
+
+playerBtn.forEach((btn, i) => {
+    console.log(btn);
+    console.log(playerBlock[i]);
     btn.addEventListener ('click', () => {
+        deactivationPlayer();
+        btn.classList.add('active');
+        playerBlock[i].classList.add('active')
 
     }) 
 })
@@ -17,3 +30,11 @@ console.log(playerBlock);
 radioPlayerInit();
 musicPlayerInit();
 videoPlayerInit();
+
+<link rel="icon" type="image/png" sizes="192x192" href="favicon/android-icon-192x192.png">
+styleshee
+t
+stylesheet
+stylesheet
+player
+player-btn player-video
